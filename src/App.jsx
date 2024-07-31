1c12a5a3e1f54402876d1d54b5220330e564c998
albope <albertobort@gmail.com>
2024-07-31 14:33:06 +0200

    Updated header, added FontAwesome icons, and improved styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import './index.css';
 import MapComponent from './components/MapComponent';
 import CountrySelector from './components/CountrySelector';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
+import '@fortawesome/fontawesome-free/css/all.min.css'; // Importa el CSS de Font Awesome
 
 const App = () => {
   return (
     <div>
       <header className="header">
         <nav className="navbar">
-          <div className="logo">
-            🌍
+          <div className="logo" style={{ display: 'flex', alignItems: 'center' }}>
+            <FontAwesomeIcon icon={faGlobe} style={{ color: 'white', marginRight: '10px', fontSize: '24px' }} />
+            <span style={{ color: 'white', fontSize: '24px', fontWeight: 'bold' }}>travel-map generator</span>
           </div>
           <div className="nav-links">
             <a href="#about">About</a>
@@ -18,7 +22,7 @@ const App = () => {
         </nav>
       </header>
       <main className="main">
-        <div className="title-container">
+        <div>
           <h1 className="main-title">Travel. Select. Generate.</h1>
           <p className="main-description">Create a personalized travel map showcasing the countries you've visited. Share your adventures with friends and plan your next destination!</p>
         </div>
@@ -30,7 +34,7 @@ const App = () => {
         </div>
       </main>
       <footer>
-        <p>&copy; 2023 Your Website</p>
+        <p>Made with <span style={{ color: 'red' }}>❤</span> by Alberto Bort</p>
       </footer>
     </div>
   );
