b1a8778b78fa4e0157b86883594fe1320525533e
albope <albertobort@gmail.com>
2024-08-14 02:42:06 +0200

    Cambios esteticos al contenedor de estadisticas II

--- src/components/StatsCard.jsx
+++ src/components/StatsCard.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGlobe, faPlane } from '@fortawesome/free-solid-svg-icons';
+import { faGlobe, faPlane, faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
 
 const StatsCard = ({ continents, countries, percentage, visitedCountries }) => {
+  const [showCountries, setShowCountries] = useState(false);
+
+  const toggleCountriesVisibility = () => {
+    setShowCountries(!showCountries);
+  };
+
   return (
     <div className="stats-container">
-      <h2 className="stats-title">My Travel Stats</h2> {/* Título centrado arriba */}
+      <h2 className="stats-title">My Travel Stats</h2>
       <div className="stats-row">
         <div className="stat-item">
           <FontAwesomeIcon icon={faGlobe} className="icon text-blue-500 mb-1" />
@@ -29,14 +35,30 @@ const StatsCard = ({ continents, countries, percentage, visitedCountries }) => {
           </div>
         </div>
       </div>
-      <div className="visited-countries-list mt-4">
-        <h3 className="font-semibold">Visited Countries:</h3>
-        <ul className="visited-countries-ul">
-          {visitedCountries.map((country, index) => (
-            <li key={index} className="text-sm">{country}</li>
-          ))}
-        </ul>
-      </div>
+      <button
+        className="toggle-button"
+        onClick={toggleCountriesVisibility}
+      >
+        {showCountries ? (
+          <>
+            Hide Visited Countries <FontAwesomeIcon icon={faChevronUp} />
+          </>
+        ) : (
+          <>
+            Show Visited Countries <FontAwesomeIcon icon={faChevronDown} />
+          </>
+        )}
+      </button>
+      {showCountries && (
+        <div className="visited-countries-list">
+          <h3 className="font-semibold mt-4">Visited Countries:</h3>
+          <ul>
+            {visitedCountries.map((country, index) => (
+              <li key={index} className="text-sm">{country}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
